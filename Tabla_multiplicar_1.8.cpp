b36dddcf8c96b587b7efd01b4d1ce66d80f7e590
luisllamasr <luislla.alumn@cescristorey.com>
2022-10-27 11:50:12 +0200

    Hola polete

---
+++ Tabla_multiplicar_1.8.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	int numero_base = 0, numerox1 = 0, numerox2 = 0, numerox3 = 0, numerox4 = 0, numerox5 = 0, numerox6 = 0, numerox7 = 0, numerox8 = 0, numerox9 = 0, numerox10 = 0;
+	
+	cout << endl << "Hola usuario, este programa te dirá la tabla de multiplicar del número que escribas" << endl << "¿Cuál va a ser el número al que le vamos a aplicar la tabla?: ";
+	cin >> numero_base;
+	
+	numerox1 = numero_base * 1;
+	numerox2 = numero_base * 2;
+	numerox3 = numero_base * 3;
+	numerox4 = numero_base * 4;
+	numerox5 = numero_base * 5;
+	numerox6 = numero_base * 6;
+	numerox7 = numero_base * 7;
+	numerox8 = numero_base * 8;
+	numerox9 = numero_base * 9;
+	numerox10 = numero_base * 10;
+	
+	cout << endl << numero_base << " x 1 = " << numerox1 << endl;
+	cout << numero_base << " x 2 = " << numerox2 << endl;
+	cout << numero_base << " x 3 = " << numerox3 << endl;
+	cout << numero_base << " x 4 = " << numerox4 << endl;
+	cout << numero_base << " x 5 = " << numerox5 << endl;
+	cout << numero_base << " x 6 = " << numerox6 << endl;
+	cout << numero_base << " x 7 = " << numerox7 << endl;
+	cout << numero_base << " x 8 = " << numerox8 << endl;
+	cout << numero_base << " x 9 = " << numerox9 << endl;
+	cout << numero_base << " x 10 = " << numerox10 << endl << endl;
+	
+}
